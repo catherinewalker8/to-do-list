1d97365a58f8f81e289868494ff27379bb02e351
LAPTOP-EURV9OG3\neerg <catwal@ceh.ac.uk>
2026-01-31 19:07:06 +0000

    added the ability to tick items off the list

--- script.js
+++ script.js
@@ -2,18 +2,42 @@ var newItem = document.getElementById("newItem");
 
 var saveButton = document.getElementById("saveButton");
 
+var nothingToDo = document.getElementById("nothingToDo");
+
+
 saveButton.addEventListener("click", onClickSaveButton);
 
 
 function onClickSaveButton() {
-  var newItemValue = newItem.value;
-  var itemsArea = document.getElementById("items");
-  var newTr = document.createElement("tr");
-  newTr.innerHTML = 
-        `<td>${newItemValue}</td>
-        <td><button class="edit"></button></td>
-        <td><button class="done"></button></td>
-        <td><button class="bin"></button></td>`;
-  itemsArea.appendChild(newTr);
-  newItem.value = null;
+    var newItemValue = newItem.value;
+        if (newItemValue.trim()==""){
+            newItem.value = null;
+            return;
+        } 
+        var itemsArea = document.getElementById("items");
+        var newTr = document.createElement("tr");
+        newTr.innerHTML = 
+                `<td><button class="done"></button></td>
+                <td>${newItemValue}</td>
+                <td><button class="edit"></button></td>
+                <td><button class="bin"></button></td>`;
+        itemsArea.appendChild(newTr);
+        newItem.value = null;
+        nothingToDo.style.display="none";
 }
+
+
+var itemsArea = document.getElementById("items");
+
+itemsArea.addEventListener("click", onClickDoneButton);
+
+function onClickDoneButton(e){
+    if (e.target.classList.contains("done")) {
+        e.target.classList.toggle("complete");
+    }
+}
+
+
+
+
+
