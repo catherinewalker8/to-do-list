335edd1a68418d5ce77d14a5aa623a9914ebc086
LAPTOP-EURV9OG3\neerg <catwal@ceh.ac.uk>
2026-02-01 09:42:59 +0000

    first attempt at using the modal to edit list items

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ var saveButton = document.getElementById("saveButton");
 
 var nothingToDo = document.getElementById("nothingToDo");
 
+var rowBeingEdited = null;
+
+var itemsArea = document.getElementById("items");
+
 
 saveButton.addEventListener("click", onClickSaveButton);
 
+itemsArea.addEventListener("click", onClickListButton);
+
 
 function onClickSaveButton() {
-    var newItemValue = newItem.value;
+        var newItemValue = newItem.value;
+        if (rowBeingEdited != null){
+            rowBeingEdited.cells[1].innerText = newItemValue;
+            rowBeingEdited = null;
+        }
+        else{
         if (newItemValue.trim() == ""){
             newItem.value = null;
             return;
@@ -20,18 +31,16 @@ function onClickSaveButton() {
                 `<tr>
                 <td><button class="done"></button></td>
                 <td>${newItemValue}</td>
-                <td><button class="edit"></button></td>
+                <td><button class="edit" data-bs-toggle="modal"></button></td>
                 <td><button class="bin"></button></td>
                 <tr>`;
         itemsArea.appendChild(newTr);
+        }
         newItem.value = null;
         nothingToDo.style.display = "none";
 }
 
 
-var itemsArea = document.getElementById("items");
-
-itemsArea.addEventListener("click", onClickListButton);
 
 function onClickListButton(e){
     if (e.target.classList.contains("done")) {
@@ -41,14 +50,27 @@ function onClickListButton(e){
         var row = e.target.closest("tr");
         row.remove();
     }
+
+    // Editing rows using the prompt box method:
+
+    // else if (e.target.classList.contains("edit")){
+    //     var row = e.target.closest("tr");
+    //     var cell = row.cells[1];
+    //     var updatedItem = prompt("Edit your task: ", cell.innerText);
+    // if (updatedItem != null && updatedItem.trim() != "") {
+    //     cell.innerText = updatedItem;
+    // }
+    // }
+
+    //Editing rows using the modal I created earlier
+
     else if (e.target.classList.contains("edit")){
-        var row = e.target.closest("tr");
         var cell = row.cells[1];
-        var updatedItem = prompt("Edit your task: ", cell.innerText);
-    if (updatedItem != null && updatedItem.trim() != "") {
-        cell.innerText = updatedItem;
+        var modalInput = document.querySelector("input");
+        modalInput.value = cell.innerText;
     }
-    }
+
+
     var itemsArea = document.getElementById("items");
     if (itemsArea.children.length === 0) {
         nothingToDo.style.display = "block";
